3ea627ad3fc131ad5809e467fb17c49656431a84
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2017-03-13 19:49:02 +0000

    Fix env var setup

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,21 +1,16 @@
 
-if "%CEF3_PATH%" == "" setx CEF3_PATH "C:\cef3-dev"
+if "%CEF3_PATH%" == "" (setx /M CEF3_PATH "C:\cef3-dev")
 if exist "%CEF3_PATH%" (goto :Done)
-echo 3
 
 curl -ocef1750_win64.7z -L https://github.com/haskell-ui/cef3-raw/releases/download/1750win64/cef1750_win64.7z
 7z x cef1750_win64.7z
 mkdir %CEF3_PATH%
-echo 4
 move cef1750_win64\* %CEF3_PATH%
-echo 5
 del cef1750_win64.7z
-echo 6
 
 dir
 
 echo %CEF3_PATH%
-echo 7
 dir %CEF3_PATH%
 
 :Done
