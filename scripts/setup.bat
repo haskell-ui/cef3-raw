2aa6a0fd1bcbd673ac37d41f0a32ce4aa16b3c7c
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2017-03-13 20:18:07 +0000

    Copy instead of move, maybe?

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -8,8 +8,9 @@ if exist "%CEF3_PATH%" (goto :Done)
 curl -ocef1750_win64.7z -L https://github.com/haskell-ui/cef3-raw/releases/download/1750win64/cef1750_win64.7z
 7z x cef1750_win64.7z
 mkdir %CEF3_PATH%
-move cef1750_win64\* %CEF3_PATH%
+xcopy /E cef1750_win64 %CEF3_PATH%
 del cef1750_win64.7z
+rmdir /S /Q cef1750_win64
 
 dir
 
