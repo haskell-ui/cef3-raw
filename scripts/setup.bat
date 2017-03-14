374f35fa8fa39b82c8b6bd343605e8c41db4a9a5
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2017-03-14 03:47:08 +0000

    Call conv test

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -6,15 +6,10 @@ if "%CEF3_PATH%" == "" (
 if exist "%CEF3_PATH%" (goto :Done)
 
 curl -ocef1750_win64.7z -L https://github.com/haskell-ui/cef3-raw/releases/download/1750win64/cef1750_win64.7z
-7z x cef1750_win64.7z
+7z x cef1750_win64.7z > NUL
 mkdir %CEF3_PATH%
 xcopy /E cef1750_win64 %CEF3_PATH%
 del cef1750_win64.7z
 rmdir /S /Q cef1750_win64
-
-dir
-
-echo CEF3_PATH: %CEF3_PATH%
-dir %CEF3_PATH%
 
 :Done
